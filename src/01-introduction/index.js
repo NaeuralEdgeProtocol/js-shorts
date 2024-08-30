c105a318dcbdf00c6120a403a06a9a22313dd718
Radu Lupaescu <radu.lupaescu@gmail.com>
2024-08-30 16:23:19 +0300

    example: loading identity;

--- src/01-introduction/index.js
+++ src/01-introduction/index.js
@@ -1,11 +1,15 @@
 import { makeConfig } from '../common/configure.js';
-import {HEARTBEATS_STREAM, Naeural} from '@naeural/jsclient';
+import {HEARTBEATS_STREAM, Naeural, NaeuralBC} from '@naeural/jsclient';
+import {readPemFile} from '../common/load.identity.js';
 
 
 const config = makeConfig(['0xai_Amfnbt3N-qg2-qGtywZIPQBTVlAnoADVRmSAsdDhlQ-6']);
 
 const naeuralClient = new Naeural(config);
+naeuralClient.loadIdentity(NaeuralBC.loadFromPem(readPemFile('naeural.pem')));
 naeuralClient.boot();
+
+console.log('MY ADDRESS: ', naeuralClient.getBlockChainAddress());
 
 naeuralClient.getStream(HEARTBEATS_STREAM).subscribe((message) => {
     const nodeName = message.data.DATA.node.EE_ID;
